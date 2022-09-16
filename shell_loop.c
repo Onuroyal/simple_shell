c95ada65400db950552df6e295d094290750f6d8
Onuroyal <onucheabah@gmail.com>
2022-09-16 03:27:57 -0700

    my_simple-shell

--- shell_loop.c
+++ shell_loop.c
@@ -4,12 +4,14 @@
  * hsh - main shell loop
  * @info: the parameter & return info struct
  * @av: the argument vector from main()
- * Return: 0 on success, 1 on error, or error code 
+ *
+ * Return: 0 on success, 1 on error, or error code
  */
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
 	int builtin_ret = 0;
+
 	while (r != -1 && builtin_ret != -2)
 	{
 		clear_info(info);
@@ -46,9 +48,9 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 0 if builtin executed successfully,
- * 1 if builtin found but not successful,
- * -2 if builtin signals exit()
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -81,11 +83,11 @@ int find_builtin(info_t *info)
  *
  * Return: void
  */
-
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
 	int i, k;
+
 	info->path = info->argv[0];
 	if (info->linecount_flag == 1)
 	{
@@ -97,6 +99,7 @@ void find_cmd(info_t *info)
 			k++;
 	if (!k)
 		return;
+
 	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
 	if (path)
 	{
@@ -106,7 +109,7 @@ void find_cmd(info_t *info)
 	else
 	{
 		if ((interactive(info) || _getenv(info, "PATH=")
-					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
 		else if (*(info->arg) != '\n')
 		{
@@ -119,9 +122,9 @@ void find_cmd(info_t *info)
 /**
  * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
+ *
  * Return: void
  */
-
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
@@ -155,4 +158,3 @@ void fork_cmd(info_t *info)
 		}
 	}
 }
-
